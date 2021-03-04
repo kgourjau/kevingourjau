19a5732cef648f9b7a266ab93f9776c0a9462039
Kevin Gourjau <kevin.gourjau@gmail.com>
2021-03-04 02:47:06 +0100

    e

---
+++ database/users.js
@@ -0,0 +1,20 @@
+import nextConnect from 'next-connect';
+import middleware from '../middleware/mongodb';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+    let doc = await req.db.collection('users').findOne()
+    res.json(doc);
+});
+
+handler.post(async (req, res) => {
+    let data = req.body;
+    let doc = await req.db.collection('users').updateOne({email: data.email}, {$set:data}, {upsert: true})
+
+    res.json({message: 'ok'});
+})
+
+export default handler;
